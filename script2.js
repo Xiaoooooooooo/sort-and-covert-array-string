cc7945df808f4310873eb3ca53811b936133d720
Xiaoooooooooo <56744364+Xiaoooooooooo@users.noreply.github.com>
2019-12-11 14:20:06 -1000

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,25 @@
+//this sorts from small to high
+/*var ages = [12,4,5,10,39,59,82,18];
+
+function sortMe() {
+  ages.sort(function(a,b) {
+    return(a-b);
+    
+  })
+  window.alert(ages)
+}
+sortMe()*/
+
+
+//this converts an array into a string
+/*var food = ["apple","banana"];
+var newarr = food.toString()
+window.alert(newarr[0]); //should return "a"*/
+
+
+
+
+//this converts string to array
+var a = "apple,banana,orange";
+var newarr = a.split(",")
+window.alert(newarr)
